ef370ff2ef94fa11fc7d230ec054276cbcda74d7
Aravindms369 <55455165+Aravindms369@users.noreply.github.com>
2020-12-16 16:39:48 +0530

    login system included

--- mysite/EventHub/urls.py
+++ mysite/EventHub/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 from . import views
+
 app_name = "EventHub"
 
 urlpatterns = [
-    
-    path("", views.homepage,name="homepage"),
+
+    path("", views.home,name="home"),
+    path("register/", views.register, name="register"),
+    path("logout/", views.logout_request, name="logout"),
+    path("login/", views.login_request, name="login"),
+    path("eventpage/", views.eventpage, name="eventpage"),
 
 
 ]
--- mysite/EventHub/views.py
+++ mysite/EventHub/views.py
@@ -1,12 +1,57 @@
-from django.shortcuts import render
 from django.shortcuts import render,redirect, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.forms import  AuthenticationForm
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from django.urls import reverse
-# Create your views here.
+from .forms import NewUserForm
+from django.utils import timezone
+from datetime import date, timedelta
+from django.db.models import Q
 
-def homepage(request):
+# Create your views here.
+def home(request):
 	return render(request=request,
 		          template_name="EventHub/home.html")
+def register(request):
+	if request.method == "POST":
+		form = NewUserForm(request.POST)
+		if form.is_valid():
+			user = form.save()
+			username = form.cleaned_data.get('username')
+			messages.success(request, f"New account created: {username}")
+			return redirect("EventHub:login")
+		else:
+			for msg in form.error_messages:
+				messages.error(request,f"{msg}:{form.error_messages[msg]}")
+	form = NewUserForm
+	return render(request,"EventHub/register.html", context={"form":form})
+def logout_request(request):
+	logout(request)
+	messages.info(request, "Logged out succesfully!")
+	return redirect("EventHub:login")
+def login_request(request):
+	if request.method == 'POST':
+		form = AuthenticationForm(request=request, data=request.POST)
+		if form.is_valid():
+			username = form.cleaned_data.get('username')
+			password = form.cleaned_data.get('password')
+			user = authenticate(username=username, password=password)
+			if user is not None:
+				login(request, user)
+				messages.info(request, f"You are now logged in as {username}")
+				return HttpResponseRedirect(reverse("EventHub:eventpage"))
+				
+				
+			else:
+				messages.error(request, "Invalid username or password.")
+		else:
+			messages.error(request, "Invalid username or password.")
+
+	form = AuthenticationForm()
+	return render(request,
+		          "EventHub/login.html",
+				  context={"form":form})
+def eventpage(request):
+    return render(request=request,
+		          template_name="EventHub/eventpage.html")
