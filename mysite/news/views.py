6dc3ac4608739c9a969daa312289407d63c67aaf
shiyask1296 <61624675+shiyask1296@users.noreply.github.com>
2020-12-20 22:35:50 +0530

    fresh

---
+++ mysite/news/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+import requests
+# Create your views here.
+def home(request):
+    return render(request,'home.html')
+def map(request):
+    return render(request,'map.html')
